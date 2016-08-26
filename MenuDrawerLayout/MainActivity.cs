a62fda9fd7f75c2390a2742e52097d9bd1d9a3b6
Yawo Echitey <yawo@echitey.com>
2016-08-26 11:35:56 -0300

    Added Support ActionBar

--- MenuDrawerLayout/MainActivity.cs
+++ MenuDrawerLayout/MainActivity.cs
@@ -5,14 +5,16 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V7.App;
 
 namespace MenuDrawerLayout
 {
     [Activity(Label = "MenuDrawerLayout", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : AppCompatActivity
     {
-        int count = 1;
 
+        private SupportToolbar mtoolbar;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -20,11 +22,10 @@ namespace MenuDrawerLayout
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            mtoolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(mtoolbar);
+            SupportActionBar.Title = "Hamburger";
+            
         }
     }
 }
