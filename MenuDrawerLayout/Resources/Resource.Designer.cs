50561782575f02d15648595113f2b302f62560e1
Yawo Echitey <yawo@echitey.com>
2016-08-26 11:58:07 -0300

    Added DrawerLayout to Main.axml

--- MenuDrawerLayout/Resources/Resource.Designer.cs
+++ MenuDrawerLayout/Resources/Resource.Designer.cs
@@ -1456,8 +1456,8 @@ namespace MenuDrawerLayout
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f0b0051
-			public const int action0 = 2131427409;
+			// aapt resource value: 0x7f0b0053
+			public const int action0 = 2131427411;
 			
 			// aapt resource value: 0x7f0b0041
 			public const int action_bar = 2131427393;
@@ -1483,8 +1483,8 @@ namespace MenuDrawerLayout
 			// aapt resource value: 0x7f0b0042
 			public const int action_context_bar = 2131427394;
 			
-			// aapt resource value: 0x7f0b0055
-			public const int action_divider = 2131427413;
+			// aapt resource value: 0x7f0b0057
+			public const int action_divider = 2131427415;
 			
 			// aapt resource value: 0x7f0b0002
 			public const int action_menu_divider = 2131427330;
@@ -1516,14 +1516,14 @@ namespace MenuDrawerLayout
 			// aapt resource value: 0x7f0b002b
 			public const int buttonPanel = 2131427371;
 			
-			// aapt resource value: 0x7f0b0052
-			public const int cancel_action = 2131427410;
+			// aapt resource value: 0x7f0b0054
+			public const int cancel_action = 2131427412;
 			
 			// aapt resource value: 0x7f0b0039
 			public const int checkbox = 2131427385;
 			
-			// aapt resource value: 0x7f0b0058
-			public const int chronometer = 2131427416;
+			// aapt resource value: 0x7f0b005a
+			public const int chronometer = 2131427418;
 			
 			// aapt resource value: 0x7f0b001d
 			public const int collapseActionView = 2131427357;
@@ -1546,14 +1546,17 @@ namespace MenuDrawerLayout
 			// aapt resource value: 0x7f0b000c
 			public const int disableHome = 2131427340;
 			
+			// aapt resource value: 0x7f0b0051
+			public const int drawer_layout = 2131427409;
+			
 			// aapt resource value: 0x7f0b0043
 			public const int edit_query = 2131427395;
 			
 			// aapt resource value: 0x7f0b001a
 			public const int end = 2131427354;
 			
-			// aapt resource value: 0x7f0b005d
-			public const int end_padder = 2131427421;
+			// aapt resource value: 0x7f0b005f
+			public const int end_padder = 2131427423;
 			
 			// aapt resource value: 0x7f0b0025
 			public const int expand_activities_button = 2131427365;
@@ -1576,14 +1579,17 @@ namespace MenuDrawerLayout
 			// aapt resource value: 0x7f0b0026
 			public const int image = 2131427366;
 			
+			// aapt resource value: 0x7f0b005e
+			public const int info = 2131427422;
+			
+			// aapt resource value: 0x7f0b0052
+			public const int left_drawer = 2131427410;
+			
+			// aapt resource value: 0x7f0b0058
+			public const int line1 = 2131427416;
+			
 			// aapt resource value: 0x7f0b005c
-			public const int info = 2131427420;
-			
-			// aapt resource value: 0x7f0b0056
-			public const int line1 = 2131427414;
-			
-			// aapt resource value: 0x7f0b005a
-			public const int line3 = 2131427418;
+			public const int line3 = 2131427420;
 			
 			// aapt resource value: 0x7f0b0009
 			public const int listMode = 2131427337;
@@ -1591,8 +1597,8 @@ namespace MenuDrawerLayout
 			// aapt resource value: 0x7f0b0028
 			public const int list_item = 2131427368;
 			
-			// aapt resource value: 0x7f0b0054
-			public const int media_actions = 2131427412;
+			// aapt resource value: 0x7f0b0056
+			public const int media_actions = 2131427414;
 			
 			// aapt resource value: 0x7f0b001b
 			public const int middle = 2131427355;
@@ -1693,8 +1699,8 @@ namespace MenuDrawerLayout
 			// aapt resource value: 0x7f0b0018
 			public const int src_over = 2131427352;
 			
-			// aapt resource value: 0x7f0b0053
-			public const int status_bar_latest_event_content = 2131427411;
+			// aapt resource value: 0x7f0b0055
+			public const int status_bar_latest_event_content = 2131427413;
 			
 			// aapt resource value: 0x7f0b004c
 			public const int submit_area = 2131427404;
@@ -1702,17 +1708,17 @@ namespace MenuDrawerLayout
 			// aapt resource value: 0x7f0b000b
 			public const int tabMode = 2131427339;
 			
-			// aapt resource value: 0x7f0b005b
-			public const int text = 2131427419;
+			// aapt resource value: 0x7f0b005d
+			public const int text = 2131427421;
 			
-			// aapt resource value: 0x7f0b0059
-			public const int text2 = 2131427417;
+			// aapt resource value: 0x7f0b005b
+			public const int text2 = 2131427419;
 			
 			// aapt resource value: 0x7f0b0034
 			public const int textSpacerNoButtons = 2131427380;
 			
-			// aapt resource value: 0x7f0b0057
-			public const int time = 2131427415;
+			// aapt resource value: 0x7f0b0059
+			public const int time = 2131427417;
 			
 			// aapt resource value: 0x7f0b002a
 			public const int title = 2131427370;
@@ -2425,6 +2431,9 @@ namespace MenuDrawerLayout
 			
 			// aapt resource value: 0x7f080079
 			public const int Base_Widget_AppCompat_Toolbar_Button_Navigation = 2131230841;
+			
+			// aapt resource value: 0x7f08013f
+			public const int MyDrawerArrowStyle = 2131231039;
 			
 			// aapt resource value: 0x7f08013e
 			public const int MyTheme = 2131231038;
